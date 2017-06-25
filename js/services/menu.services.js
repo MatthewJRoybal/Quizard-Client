352eb79e0e4289c49aff502fa78e94200c19d79a
Matthew J Roybal <matthewjroybal@gmail.com>
2017-06-24 20:52:25 -0700

    update

--- js/services/menu.services.js
+++ js/services/menu.services.js
@@ -1,18 +1,16 @@
-
 function checkPosition() {
-    if (window.matchMedia('(max-width: 899px)').matches) {
-        $( "#nav-menu" ).hide();
-        $( ".cross" ).hide();
-    } else if (window.matchMedia('(min-width: 900px)').matches) {
-        $( "#nav-menu" ).show();
-        $( ".cross" ).hide();
-    }
+  if (window.matchMedia('(max-width: 899px)').matches) {
+    $( "#nav-menu" ).hide();
+    $( ".cross" ).hide();
+  } else if (window.matchMedia('(min-width: 900px)').matches) {
+    $( "#nav-menu" ).show();
+    $( ".cross" ).hide();
+  }
 }
 
 $( ".hamburger" ).click(function() {
   $("header").animate({'margin-bottom': '220px'}, 500);
   $( "#nav-menu" ).slideToggle( 500, function() {
-
     $( ".hamburger" ).hide();
     $( ".cross" ).show();
   });
@@ -21,7 +19,6 @@ $( ".hamburger" ).click(function() {
 $( ".cross" ).click(function() {
   $("header").animate({'margin-bottom': '0px'}, 500);
   $( "#nav-menu" ).slideToggle( 500, function() {
-      
       $( ".cross" ).hide();
       $( ".hamburger" ).show();
   });
