546f7700c53e10ce05cf25159a077f8972e00609
MatthewJRoybal <matthewjroybal@gmail.com>
2017-08-02 11:39:15 -0700

    updating

--- js/services/User.services.js
+++ js/services/User.services.js
@@ -57,7 +57,7 @@ function createUser(userObject) {
 			success: function(user) {
 				loginUser(userObject);
 			},
-			contentType: "application/x-www-form-urlencoded" // "application/json"
+			contentType: "application/json"
 		});
 }
 
@@ -78,7 +78,7 @@ function loginUser(userObject) {
         window.location.href = "../html/results.html";
 				window.localStorage.setItem('token', user.token);
 			},
-			contentType: "application/x-www-form-urlencoded", // "application/json",
+			contentType: "application/json",
       xhrFields: {
         withCredentials: true
       },
